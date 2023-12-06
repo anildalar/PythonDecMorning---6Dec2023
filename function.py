c47a89fb4cd9024fcd83bc6420ba28ab10437f3c
anildalar <dollorinfotech@gmail.com>
2023-12-06 08:53:23 +0530

    second commit

---
+++ function.py
@@ -0,0 +1,12 @@
+#1 Function defination
+def ashish():
+    print("Please bring a glass of water. ")
+    
+#2. Function Calling/Invoking
+ashish()
+
+#1. Function defination
+def sharukh():
+    print("Please go to market.")
+
+sharukh()
